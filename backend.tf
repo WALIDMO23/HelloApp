8c8bc3ab5965003e9e66249491c765155adc0595
WALIDMO23 <121633061+WALIDMO23@users.noreply.github.com>
2025-11-18 20:52:43 +0200

    Update S3 bucket and region in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "walidmo-devops-project-repo"
+    bucket       = "welo-s3-bucket"
     key          = "eks/terraform.tfstate"
-    region       = "eu-west-1"
+    region       = "us-east-1"
     encrypt      = true
     use_lockfile = true
   }
